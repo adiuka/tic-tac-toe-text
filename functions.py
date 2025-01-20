14b8d63d885a5d5818b49b54be5d6fb27cc9c633
adiuka <amartinaitiz@gmail.com>
2025-01-20 19:47:04 +0100

    implement cpu

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 game_board = ['-', '-', '-',
               '-', '-', '-',
@@ -72,7 +73,7 @@ def check_score(b):
         return False
     
 
-def game(b):
+def two_player_game(b):
     game_board = b
     game_is_on = True
     turns = 9
@@ -87,7 +88,6 @@ def game(b):
                 else: 
                     game_board[int(player_1_choice) - 1] = 'X'
                     turns -= 1
-                    print(turns)
                     break
             except (IndexError, ValueError) as e:
                 print(f"Invalid entry: {e}. Please enter a character ranging 1 to 9. ")
@@ -125,3 +125,149 @@ def game(b):
             if check_score(game_board) == True:
                 game_is_on = False
             
+
+def against_cpu(b):
+    game_board = b
+    game_is_on = True
+    turns = 9
+    print("Lets start ze game!\n")
+    while game_is_on:
+        while True:
+            try:
+                player_1_choice = input("Player 1. Please input where you would like to place your X. Coordinates are decided by 1 to 9, from left to right: " )
+
+                if game_board[int(player_1_choice) - 1] != '-':
+                    print("Space already taken, please pick another block.")
+                else: 
+                    game_board[int(player_1_choice) - 1] = 'X'
+                    turns -= 1
+                    break
+            except (IndexError, ValueError) as e:
+                print(f"Invalid entry: {e}. Please enter a character ranging 1 to 9. ")
+
+        os.system('cls||clear') # Clears the other text so it is not cluttered
+        show_board(game_board)
+
+    # A quick check if there are more turns and for the score
+        if turns == 0:
+            game_is_on = False
+            print("The Game is a tie")
+        elif check_score(game_board) == True:
+            game_is_on = False
+        else:
+            while True:
+                # Checks the rows to block me winning
+                if game_board[0] == 'X' and game_board[1] == 'X' and game_board[2] == '-':
+                    game_board[2] = 'O'
+                    turns -= 1
+                    break
+                elif game_board[1] == 'X' and game_board[2] == 'X' and game_board[0] == '-':
+                    game_board[0] = 'O'
+                    turns -= 1
+                    break
+                elif game_board[0] == 'X' and game_board[2] == 'X' and game_board[1] == '-':
+                    game_board[1] = 'O'
+                    turns -= 1
+                    break
+                elif game_board[3] == 'X' and game_board[4] == 'X' and game_board[5] == '-':
+                    game_board[5] = 'O'
+                    turns -= 1
+                    break
+                elif game_board[3] == 'X' and game_board[5] == 'X' and game_board[4] == '-':
+                    game_board[4] = 'O'
+                    turns -= 1
+                    break
+                elif game_board[4] == 'X' and game_board[5] == 'X' and game_board[3] == '-':
+                    game_board[3] = 'O'
+                    turns -= 1
+                    break
+                elif game_board[6] == 'X' and game_board[7] == 'X' and game_board[8] == '-':
+                    game_board[8] = 'O'
+                    turns -= 1
+                    break
+                elif game_board[6] == 'X' and game_board[8] == 'X' and game_board[7] == '-':
+                    game_board[7] = 'O'
+                    turns -= 1
+                    break
+                elif game_board[7] == 'X' and game_board[8] == 'X' and game_board[6] == '-':
+                    game_board[6] = 'O'
+                    turns -= 1
+                    break
+                # Checks the columns to stop me winning
+                elif game_board[0] == 'X' and game_board[3] == 'X' and game_board[6] == '-':
+                    game_board[6] = 'O'
+                    turns -= 1
+                    break
+                elif game_board[0] == 'X' and game_board[6] == 'X' and game_board[3] == '-':
+                    game_board[3] = 'O'
+                    turns -= 1
+                    break
+                elif game_board[3] == 'X' and game_board[6] == 'X' and game_board[0] == '-':
+                    game_board[0] = 'O'
+                    turns -= 1
+                    break
+                elif game_board[1] == 'X' and game_board[4] == 'X' and game_board[7] == '-':
+                    game_board[7] = 'O'
+                    turns -= 1
+                    break
+                elif game_board[1] == 'X' and game_board[7] == 'X' and game_board[4] == '-':
+                    game_board[4] = 'O'
+                    turns -= 1
+                    break
+                elif game_board[7] == 'X' and game_board[4] == 'X' and game_board[1] == '-':
+                    game_board[1] = 'O'
+                    turns -= 1
+                    break
+                elif game_board[2] == 'X' and game_board[5] == 'X' and game_board[8] == '-':
+                    game_board[8] = 'O'
+                    turns -= 1
+                    break
+                elif game_board[2] == 'X' and game_board[8] == 'X' and game_board[5] == '-':
+                    game_board[5] = 'O'
+                    turns -= 1
+                    break
+                elif game_board[5] == 'X' and game_board[8] == 'X' and game_board[2] == '-':
+                    game_board[2] = 'O'
+                    turns -= 1
+                    break
+                # Checks diagnals to prevent me from winning
+                elif game_board[0] == 'X' and game_board[4] == 'X' and game_board[8] == '-':
+                    game_board[8] = 'O'
+                    turns -= 1
+                    break
+                elif game_board[0] == 'X' and game_board[8] == 'X' and game_board[4] == '-':
+                    game_board[4] = 'O'
+                    turns -= 1
+                    break
+                elif game_board[8] == 'X' and game_board[4] == 'X' and game_board[0] == '-':
+                    game_board[0] = 'O'
+                    turns -= 1
+                    break
+                elif game_board[2] == 'X' and game_board[4] == 'X' and game_board[6] == '-':
+                    game_board[6] = 'O'
+                    turns -= 1
+                    break
+                elif game_board[2] == 'X' and game_board[6] == 'X' and game_board[4] == '-':
+                    game_board[4] = 'O'
+                    turns -= 1
+                    break
+                elif game_board[6] == 'X' and game_board[4] == 'X' and game_board[2] == '-':
+                    game_board[2] = 'O'
+                    turns -= 1
+                    break
+                else:
+                    cpu_choice = random.randint(1, 9)
+
+                    if game_board[cpu_choice - 1] != '-':
+                        print("Oops.")
+                    else:
+                        game_board[cpu_choice - 1] = 'O'
+                        turns -= 1
+                        break
+
+
+            os.system('cls||clear') # Clears the other text so it is not cluttered
+            show_board(game_board)
+            # Check the score:
+            if check_score(game_board) == True:
+                game_is_on = False
--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 from tkinter import * # This is a text based version, but will aim to make it in GUI as well.
 import functions as f
 
-game_board = ['-', '-', '-',
-              '-', '-', '-',
-              '-', '-', '-',]
 
 print("Hello and welcome to the text based tic tac toe 2 player game!\n" 
       "Instructions are very simple, when each player is promted, enter a number between 1 and 9, \n"
-      "symbolising the 9 blocks counting from 1 to 9. Currently text based, working to implement GUI.\n"
+      "symbolising the 9 blocks counting from 1 to 9. You can play against your friends or a CPU...\n"
       "Enjoyyyy!\n")
 
 
 while True:
     answer = input("Would you like to play a game of Tic Tac Toe? y for yes and n for no: ")
     if answer.lower() == 'y':
-        f.game(game_board)
+        while True:
+            answer2 = input("Select game mode (1 for single player, 2 for multiplayer and q for quit): ")
+            if answer2 == '2':
+                game_board = ['-', '-', '-',
+                              '-', '-', '-',
+                              '-', '-', '-',]
+                f.two_player_game(game_board)
+            elif answer2 == '1':
+                game_board = ['-', '-', '-',
+                              '-', '-', '-',
+                              '-', '-', '-',]
+                f.against_cpu(game_board)
+            elif answer2 == 'q':
+                break
+            else:
+                print("That is an incorrect input!")
+
     elif answer.lower() == 'n':
         print("Well... Then have a wonderful day!")
         break
